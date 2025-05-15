1c5d0fd17554bcaa5bb254a70f80ac83d70aac72
victorcreativee <tuyizerevictor@gmail.com>
2025-05-15 19:49:33 +0200

    validation fix

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -10,10 +10,10 @@ def validUTF8(data):
     :param data: List of integers
     :return: True if valid UTF-8, False otherwise
     """
-    n_bytes = 0  
+    n_bytes = 0
 
     for num in data:
-        byte = num & 0xFF 
+        byte = num & 0xFF
 
         if n_bytes == 0:
             if (byte >> 7) == 0:
@@ -25,10 +25,10 @@ def validUTF8(data):
             elif (byte >> 3) == 0b11110:
                 n_bytes = 3
             else:
-                return False  
+                return False
         else:
             if (byte >> 6) != 0b10:
                 return False
             n_bytes -= 1
 
-    return n_bytes == 0  
+    return n_bytes == 0
