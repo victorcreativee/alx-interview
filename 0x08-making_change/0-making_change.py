b051693633cc12bf0cc8db78fc03308d1967404f
victorcreativee <tuyizerevictor@gmail.com>
2025-06-13 00:27:54 +0200

    admin

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -9,7 +9,7 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    # Initialize a list for storing minimum coins needed for each value up to total
+    # Initialize a list for storing minimum
     dp = [float('inf')] * (total + 1)
     dp[0] = 0  # Base case: 0 coins to make 0
 
