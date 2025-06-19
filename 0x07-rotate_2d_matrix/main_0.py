3c866ed1f76c7c8b34ac02c2ed2240d1955d7fe1
victorcreativee <tuyizerevictor@gmail.com>
2025-06-19 17:45:38 +0200

    0-island_perimeter.py

--- 0x07-rotate_2d_matrix/main_0.py
+++ 0x07-rotate_2d_matrix/main_0.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
     matrix = [[1, 2, 3],
               [4, 5, 6],
               [7, 8, 9]]
-
+              
     rotate_2d_matrix(matrix)
     print(matrix)
---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Island Perimeter Calculator Module
+"""
+
+def island_perimeter(grid):
+    """
+    Calculate the perimeter of the island in a grid.
+
+    Args:
+        grid (list of list of int): 2D grid representing water (0) and land (1)
+
+    Returns:
+        int: The perimeter of the island
+    """
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                # Start with 4 edges
+                perimeter += 4
+                # Subtract edges that are shared with adjacent land cells
+                if i > 0 and grid[i - 1][j] == 1:  # Top neighbor
+                    perimeter -= 2
+                if j > 0 and grid[i][j - 1] == 1:  # Left neighbor
+                    perimeter -= 2
+    return perimeter
