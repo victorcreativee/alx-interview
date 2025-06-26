344118963996ed78e5ad0196c2d932cf06f1a136
victorcreativee <tuyizerevictor@gmail.com>
2025-06-26 23:18:23 +0200

    final

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,44 +1,39 @@
 #!/usr/bin/python3
-
 def sieve(n):
-    """Sieve of Eratosthenes to count primes up to n."""
-    primes = [True] * (n + 1)
+    primes = [True for _ in range(n + 1)]
     primes[0] = primes[1] = False
-
     for i in range(2, int(n ** 0.5) + 1):
         if primes[i]:
             for j in range(i * i, n + 1, i):
                 primes[j] = False
-
-    # Count cumulative primes up to n
-    prime_count = [0] * (n + 1)
     count = 0
+    result = [0] * (n + 1)
     for i in range(n + 1):
         if primes[i]:
             count += 1
-        prime_count[i] = count
-    return prime_count
+        result[i] = count
+    return result
 
 
 def isWinner(x, nums):
-    """Determine who is the winner of each game round."""
     if x < 1 or not nums:
         return None
 
     max_n = max(nums)
     prime_count = sieve(max_n)
 
-    maria_wins = 0
-    ben_wins = 0
+    maria = 0
+    ben = 0
 
-    for n in nums:
-        if prime_count[n] % 2 == 1:
-            maria_wins += 1
+    for i in range(x):
+        if prime_count[nums[i]] % 2 == 0:
+            ben += 1
         else:
-            ben_wins += 1
+            maria += 1
 
-    if maria_wins > ben_wins:
+    if maria > ben:
         return "Maria"
-    elif ben_wins > maria_wins:
+    elif ben > maria:
         return "Ben"
-    return None
+    else:
+        return None
